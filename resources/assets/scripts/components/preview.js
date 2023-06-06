41952068389a9f4ffae305e9d8ea56b4c06aede5
Tomasz Guz <tomasz.guz@bold.dev>
2023-06-06 16:32:05 +0200

    Working commit

--- resources/assets/scripts/components/preview.js
+++ resources/assets/scripts/components/preview.js
@@ -1,6 +1,4 @@
 export const preview = () => {
-  let root = document.documentElement;
-
   setTimeout(() => {
     const wrapper = document.querySelector('[data-color-inputs]');
 
@@ -8,31 +6,74 @@ export const preview = () => {
       return;
     }
 
-    const items = wrapper.querySelectorAll('[data-color-input]');
+    const colorItems = wrapper.querySelectorAll('[data-color-input]');
+    const cornersOptions = document.querySelector('[data-corners]');
+    const root = document.documentElement;
 
-    items.forEach((item) => {
-      const input = item.querySelector('.wp-color-picker');
-      const picker = item.querySelector('.wp-color-result');
+    if (! colorItems) {
+      return;
+    }
 
-      if (input) {
-        root.style.setProperty(`--${input.name}`, input.value);
+    updatePreviewColor(root, colorItems);
 
-        const options = {
-          attributes: true
-        }
+    if (! cornersOptions) {
+      return;
+    }
 
-        function callback(mutationList, observer) {
-          mutationList.forEach(function(mutation) {
-            console.log('mutation kurwa');
+    const cornerInputs = cornersOptions.querySelectorAll('input');
+    const sfcf = document.querySelector('[data-sfcf]');
 
-            const newValue = input.value;
-            root.style.setProperty(`--${input.name}`, newValue);
-          })
-        }
-
-        const observer = new MutationObserver(callback);
-        observer.observe(picker, options);
-      }
-    });
+    updateCornersStyle(sfcf, cornerInputs);
   }, 1000);
 };
+
+function updatePreviewColor(root, items) {
+  items.forEach((item) => {
+    const input = item.querySelector('.wp-color-picker');
+    const picker = item.querySelector('.wp-color-result');
+
+    if (input) {
+      root.style.setProperty(`--${input.name}`, input.value);
+
+      const options = {
+        attributes: true
+      }
+
+      function callback(mutationList, observer) {
+        mutationList.forEach(function(mutation) {
+          const newValue = input.value;
+          root.style.setProperty(`--${input.name}`, newValue);
+        })
+      }
+
+      const observer = new MutationObserver(callback);
+      observer.observe(picker, options);
+    }
+  });
+}
+
+function updateCornersStyle(main, items) {
+  items.forEach((input) => {
+    if (input.id == 'sfcf-corners-square-true' && input.checked) {
+      main.style.setProperty('--sfcf-radius-huge', 0 + 'px');
+      main.style.setProperty('--sfcf-radius-big', 0 + 'px');
+      main.style.setProperty('--sfcf-radius-medium', 0 + 'px');
+      main.style.setProperty('--sfcf-radius-small', 0 + 'px');
+      console.log('zereeo');
+    }
+
+    input.addEventListener('change', () => {
+      if (input.id == 'sfcf-corners-square-true' && input.value == 'true') {
+        main.style.setProperty('--sfcf-radius-huge', 0 + 'px');
+        main.style.setProperty('--sfcf-radius-big', 0 + 'px');
+        main.style.setProperty('--sfcf-radius-medium', 0 + 'px');
+        main.style.setProperty('--sfcf-radius-small', 0 + 'px');
+      } else {
+        main.style.setProperty('--sfcf-radius-huge', 32 + 'px');
+        main.style.setProperty('--sfcf-radius-big', 20 + 'px');
+        main.style.setProperty('--sfcf-radius-medium', 16 + 'px');
+        main.style.setProperty('--sfcf-radius-small', 6 + 'px');
+      }
+    });
+  });
+}
