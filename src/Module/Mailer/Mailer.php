b2a34e5151a3a2f0860df1c8dc75173e3706592c
Tomasz Guz <tomasz.guz@bold.dev>
2023-07-05 15:38:20 +0200

    Working commit

--- src/Module/Mailer/Mailer.php
+++ src/Module/Mailer/Mailer.php
@@ -185,11 +185,11 @@ if(!class_exists('Mailer')) {
         $body = str_replace($key, $value, $body);
       }
 
-      if (! empty($data['subject_list'])) {
-        $this->messageSubject = $data['subject_list'];
-
+      if (is_numeric($data['subject'])) {
+        $selected = $data['subject'] - 1;
         $options = explode(PHP_EOL, get_option('sfcf-form-email-subject-list'));
-        //dopasować value do opcji z adminki
+
+        $this->messageSubject = $options[$selected];
       } else {
         $this->messageSubject = $data['subject'];
       }
